ec97f273f7565da4f8092d803ae3e261c100363a
sergiohut <sergiosantiagocarmona@gmail.com>
2023-02-24 14:35:26 +0100

    profile ui

--- src/ui/Buttons.jsx
+++ src/ui/Buttons.jsx
@@ -1,15 +1,15 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 //Definir los estilos del Button mediante un template
 const ButtonStyled = styled.button`
   background-color: ${({ variant }) =>
-    variant === "contained" ? "cadetblue" : "transparent"};
-  border: ${({ variant }) => (variant === "text" ? "none" : "4px solid black")};
+    variant === 'contained' ? 'cadetblue' : 'transparent'};
+  border: ${({ variant }) => (variant === 'text' ? 'none' : '4px solid black')};
   width: fit-content;
-  font-family: "Helvetica";
+  font-family: 'Helvetica';
   font-size: 1rem;
   font-weight: 900;
-  color: ${({ color }) => (color ? color : "black")};
+  color: ${({ color }) => (color ? color : 'black')};
   letter-spacing: 3px;
   outline: none;
   padding: 1rem 2rem;

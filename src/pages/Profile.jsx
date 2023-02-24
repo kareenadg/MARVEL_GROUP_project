231c9040b23d3da76f2b73b380407da9d6691e4c
sergiohut <sergiosantiagocarmona@gmail.com>
2023-02-24 13:18:07 +0100

    feat profilePage UI components

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,58 @@
+import Avatar from '../ui/avatar';
 import './Profile.css';
-
-import React from 'react';
+import { useState } from 'react';
+import Button from '../ui/Buttons';
 
 const Profile = () => {
-  return <div>Profile</div>;
+  const [showFavourites, setShowFavourites] = useState(true);
+  const [showWatchlist, setShowWatchlist] = useState(false);
+  const [showComments, setShowComments] = useState(false);
+
+  return (
+    <main className="profile">
+      <Avatar
+        name={localStorage.getItem('user')}
+        image="https://res.cloudinary.com/ddu2qdsdp/image/upload/v1677237031/Marvel/photo-1438761681033-6461ffad8d80_cuvfkr.jpg"
+        size="lg"
+      />
+      <h2>{localStorage.getItem('user')}</h2>
+      <div className="options">
+        <Button
+          text="Favourites"
+          action={() => {
+            setShowFavourites(true), setShowWatchlist(false), setShowComments(false);
+          }}
+        />
+
+        <Button
+          text="Watchlist"
+          action={() => {
+            setShowFavourites(false), setShowWatchlist(true), setShowComments(false)
+            ;
+          }}
+        />
+
+        <Button
+          text="Comments"
+          action={() => {
+            setShowFavourites(false), setShowWatchlist(false), setShowComments(true);
+          }}
+        />
+      </div>
+
+      {showFavourites && (
+        <p>Aqui habría que hacer una petición al enpoint con los favoritos</p>
+      )}
+
+      {showWatchlist && (
+        <p>Aqui habría que hacer una petición al enpoint con la watchlist</p>
+      )}
+
+      {showComments && (
+        <p>Aqui habría que hacer una petición al enpoint con los comentarios</p>
+      )}
+    </main>
+  );
 };
 
 export default Profile;
---
+++ src/ui/Avatar.jsx
@@ -0,0 +1,46 @@
+import styled from 'styled-components';
+
+const AvatarStyled = styled.div`
+  width: ${({ size }) =>
+    size === 'xl' ? '300px' : size === 'lg' ? '200px' : size === 'md' ? '100px' : '40px'};
+  height: ${({ size }) =>
+    size === 'xl' ? '300px' : size === 'lg' ? '200px' : size === 'md' ? '100px' : '40px'};
+  border-radius: 50%;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  font-size: ${({ size }) =>
+    size === 'xl' ? '3rem' : size === 'lg' ? '2rem' : size === 'md' ? '1.5rem' : '1rem'};
+  background-color: cadetblue;
+  & img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`;
+
+const Avatar = ({ image = '', name = 'Hola Adios', size }) => {
+  const generateInitials = (name) => {
+    const splittedName = name.split(' ');
+    const initials = splittedName[0][0] + ' ' + splittedName[1][0];
+    return initials;
+  };
+
+  if (image) {
+    return (
+      <AvatarStyled size={size}>
+        <img src={image} alt={name} />
+      </AvatarStyled>
+    );
+  } else {
+    return (
+      <AvatarStyled size={size}>
+        <h2>{generateInitials(name)}</h2>
+      </AvatarStyled>
+    );
+  }
+};
+
+export default Avatar;

a0967ee109534c348b316a9456d341a88617b51a
jennymaow <maojenny172gmail.com>
2023-02-25 10:32:45 +0100

    saving.forum

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,8 +1,8 @@
 import './Profile.css';
 
 import { useEffect, useState } from 'react';
-import useFetch from "../hooks/useFetch"
 
+import useFetch from '../hooks/useFetch';
 import Avatar from '../ui/avatar';
 import Button from '../ui/Buttons';
 
@@ -12,9 +12,8 @@ const Profile = () => {
   const [showComments, setShowComments] = useState(false);
   const [comments, setComments] = useState([]);
   const [filteredComments, setFilteredComments] = useState([]);
-  const [favs] = useFetch("https://63ef88eb4d5eb64db0cbc71f.mockapi.io/movies");
-  const [watchlist] = useFetch("https://63ef88eb4d5eb64db0cbc71f.mockapi.io/movies");
-
+  const [favs] = useFetch('https://63ef88eb4d5eb64db0cbc71f.mockapi.io/movies');
+  const [watchlist] = useFetch('https://63ef88eb4d5eb64db0cbc71f.mockapi.io/movies');
 
   const getForumInfo = async () => {
     const res = await fetch('https://63f885816978b1f9105b3d9e.mockapi.io/reviews');
@@ -30,15 +29,14 @@ const Profile = () => {
 
   const deleteComments = (id) => {
     fetch(`https://63f885816978b1f9105b3d9e.mockapi.io/reviews/${id}`, {
-      method: "DELETE",
+      method: 'DELETE',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
     }).then((res) => {
       getForumInfo();
     });
   };
-
 
   useEffect(() => {
     localStorage.setItem(
@@ -77,20 +75,34 @@ const Profile = () => {
               setShowWatchlist(false),
               setShowComments(true),
               filterComments(localStorage.getItem('user'));
-          }}>Comments</button>
+          }}
+        >
+          Comments
+        </button>
       </div>
 
       {showFavourites && (
         <div>
-          {favs && favs.map((item) => <h2 key={item.id}> {item.title} ({item.phase})</h2>)}
+          {favs &&
+            favs.map((item) => (
+              <h2 key={item.id}>
+                {' '}
+                {item.title} ({item.phase})
+              </h2>
+            ))}
         </div>
       )}
 
       {showWatchlist && (
         <div>
-        <div className="movies-container">
-        {favs && favs.map((item) => <figure key={item.id}><img src={item.poster} alt={item.title}/></figure>)}
-        </div>
+          <div className="movies-container">
+            {favs &&
+              favs.map((item) => (
+                <figure key={item.id}>
+                  <img src={item.poster} alt={item.title} />
+                </figure>
+              ))}
+          </div>
         </div>
       )}
 

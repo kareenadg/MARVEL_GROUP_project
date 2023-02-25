fddb7ce0fb9b9d3f7b51d1b46d9eb5291cca50e5
jennymaow <maojenny172gmail.com>
2023-02-25 17:02:30 +0100

    prueba carrusel 2

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -3,36 +3,41 @@ import './Carousel.css';
 import React from 'react';
 import { useState } from 'react';
 const Carousel = ({ images }) => {
-  const [current, setCurrent] = useState(0);
+  const [current, setCurrent] = useState(1);
+  const nextImage = () => {
+    setCurrent(current + 1);
+  };
+
+  const prevImage = () => {
+    setCurrent(current - 1);
+    console.log(current);
+  };
   return (
     <section className="carousel-news">
-      {images.map((item, index) => (
-        <div
-          key={index}
-          className={
-            index == current ? 'carousel-card carousel-card-active' : 'carousel-card'
-          }
-        >
-          <img src={item.image} alt={item.title} />
-          <div className="card-overlay">
-            <h2>{item.title}</h2>
-            <h3>{item.subtitle}</h3>
+      <div className="carousel-wrapper">
+        {images.map((item) => (
+          <div key={item.id} className="carousel-card">
+            <img src={item.image} alt={item.title} className="card-image" />
+            <div className="card-overlay">
+              <h2 className="card-title">{item.title}</h2>
+              <h3>{item.subtitle}</h3>
+            </div>
           </div>
+        ))}
+        <div className="carousel-buttons">
+          <button onClick={() => prevImage()}>
+            <img
+              src="https://res.cloudinary.com/dnb4ujbgr/image/upload/v1675533027/Pokemons%20icons/arrow_back_chevron_direction_left_navigation_right_icon_123223_yll6zu.png"
+              alt="prev icon"
+            />
+          </button>
+          <button onClick={() => nextImage()}>
+            <img
+              src="https://res.cloudinary.com/dnb4ujbgr/image/upload/v1675533027/Pokemons%20icons/arrows_chevron_direction_left_move_next_right_icon_123222_kogexs.png"
+              alt="next icon"
+            />
+          </button>
         </div>
-      ))}
-      <div className="carousel-buttons">
-        <button>
-          <img
-            src="https://res.cloudinary.com/dnb4ujbgr/image/upload/v1675533027/Pokemons%20icons/arrow_back_chevron_direction_left_navigation_right_icon_123223_yll6zu.png"
-            alt="prev icon"
-          />
-        </button>
-        <button>
-          <img
-            src="https://res.cloudinary.com/dnb4ujbgr/image/upload/v1675533027/Pokemons%20icons/arrows_chevron_direction_left_move_next_right_icon_123222_kogexs.png"
-            alt="next icon"
-          />
-        </button>
       </div>
     </section>
   );
